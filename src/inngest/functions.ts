ce05d86344516a09857fe69ed699e8ab4333077e
Akash Deep <akash.de117@gmail.com>
2025-08-24 14:13:09 +0530

    ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,22 @@
+import { gemini, createAgent } from '@inngest/agent-kit';
+
 import { inngest } from './client';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
-  async ({ event, step }) => {
-    await step.sleep('wait-a-moment', '30s');
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: 'code-agent',
+      system:
+        'You are an expert next.js developer.  You write readable, maintainable and reusable code. You write simple Next.js & React snippets',
+      model: gemini({ model: 'gemini-2.0-flash' }),
+    });
 
-    await step.sleep('wait-a-moment', '10s');
-    return { message: `Hello ${event.data.email}!` };
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    return { output };
   }
 );
